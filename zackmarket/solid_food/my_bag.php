01e77613540d46dd534bca253428984f6e631262
isaacrochaoliveira <120681524+isaacrochaoliveira@users.noreply.github.com>
2023-09-30 13:57:08 -0300

    Update my_bag.php

--- zackmarket/solid_food/my_bag.php
+++ zackmarket/solid_food/my_bag.php
@@ -9,5 +9,11 @@ $query = $pdo->query("SELECT * FROM solid_food WHERE id_sf = '$id'");
 $res = $query->fetchAll(PDO::FETCH_ASSOC);
 if (count($res) > 0) {
     $res = $pdo->prepare("INSERT INTO my_bag SET user_id = :user_id, products_id = :products_id, category = :category, qtde = :qtde, date_purchase = :date_purchase, time_purchase = :time_purchase, payment = :payment");
-    $res->bindValue('')
+    $res->bindValue(':user_id', $_SESSION['id'], PDO::PARAM_INT);
+    $res->bindValue(':products_id', $id, PDO::PARAM_INT);
+    $res->bindValue(':category', 'Solid Food', PDO::PARAM_STR);
+    $res->bindValue(':qtde', $qtde, PDO::PARAM_INT);
+    $res->bindValue(':date_purchase', Date('Y-m-d'), PDO::PARAM_STR);
+    $res->bindValue(':time_purchase', Date('H:i'), PDO::PARAM_STR);
+    $res->bindValue(':payment', )
 }
